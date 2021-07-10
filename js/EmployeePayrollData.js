277de904a6a649bf5713a142a0dc58a32bc19058
faizanpro77 <faizyshaikh204@gmail.com>
2021-07-10 15:18:41 +0530

    MODIFIED: store in local storage

--- js/EmployeePayrollData.js
+++ js/EmployeePayrollData.js
@@ -27,11 +27,23 @@ window.addEventListener('DOMContentLoaded', (event) => {
 const save = () => {
     try {
         let employeePayrollData = createEmployeePayroll();
+        createAndUpdateStorage(employeePayrollData);
     } catch (error) {
         return;
     }
 }
 
+
+function createAndUpdateStorage(employeePayrollData) {
+    let employeePayrollList = JSON.parse(localStorage.getItem("EmployeePayrollList"));
+    if(employeePayrollList!=undefined)
+        employeePayrollList.push(employeePayrollData);
+    else {
+        employeePayrollList = [employeePayrollData];
+    }
+    alert(employeePayrollList.toString());    
+    localStorage.setItem("EmployeePayrollList", JSON.stringify(employeePayrollList));
+}
 const createEmployeePayroll = () => {
     let employeePayrollData = new EmployeePayrollData();
     try {
