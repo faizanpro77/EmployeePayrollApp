4ae3509481710e6ae55ac1e454ee54f902ac3652
faizanpro77 <faizyshaikh204@gmail.com>
2021-07-12 11:35:24 +0530

    MODIFIED: Display Employee Details from Local Storage

--- js/home.js
+++ js/home.js
@@ -1,6 +1,13 @@
+let employeePayrollList;
 window.addEventListener('DOMContentLoaded', () => {
+    employeePayrollList = getEmployeePayrollDataFromStorage();
+    document.querySelector(".emp-count").textContent = employeePayrollList.length;
     createInnerHtml();
 });
+
+const getEmployeePayrollDataFromStorage = () => {
+    return localStorage.getItem('EmployeePayrollList') ? JSON.parse(localStorage.getItem('EmployeePayrollList')) : [];
+}
 
 //UC3_Employee Payroll details in a Tabular Format.using Template Literals.
 const createInnerHtml = () => {
@@ -13,9 +20,9 @@ const createInnerHtml = () => {
     <th>Start Date</th>
     <th>Actions</th>
     </tr>`;
-    //UC4_Display All Employee Payroll Details from JSON Object
+   
+    //Display Employee Details from Local Storage
     let innerHtml = `${headerHtml}`;
-    let employeePayrollList = createEmployeePayrollJson();
     for(const employeePayrollData of employeePayrollList){
         innerHtml = `${innerHtml}
         <tr>
@@ -24,7 +31,7 @@ const createInnerHtml = () => {
             <td>${employeePayrollData._gender}</td>
             <td>${getDeptHtml(employeePayrollData._department)}</td>
             <td>${employeePayrollData._salary}</td>
-            <td>${employeePayrollData._startDate}</td>
+            <td>${formatDate(employeePayrollData._startDate)}</td>
             <td>
                 <img name="${employeePayrollData._id}" onclick="remove()" alt="delete" src="../assets/icons/delete-black-18dp.svg">
                 <img name="${employeePayrollData._id}" onclick="update()" alt="edit" src="../assets/icons/create-black-18dp.svg">
@@ -42,35 +49,12 @@ const getDeptHtml = (deptList) => {
     return deptHtml;
 }
 
-const createEmployeePayrollJson = () => {
-    let employeePayrollListLocal = [
-        {
-            _name: 'Rachel Green',
-            _gender: 'Female',
-            _department: [
-                'HR',
-                'Sales'
-            ],
-            _salary: '350000',
-            _startDate: '29 Oct 2020',
-            _note: 'This is Rachel',
-            _id: new Date().getTime(),
-            _profilePic: '../assets/profile-images/Ellipse -1.png'
-        },
-        {
-            _name: 'Chandler Bing',
-            _gender: 'Male',
-            _department: [
-                'Engineering',
-            ],
-            _salary: '400000',
-            _startDate: '9 Nov 2020',
-            _note: 'This is Chandler',
-            _id: new Date().getTime(),
-            _profilePic: '../assets/profile-images/Ellipse -3.png'
-        }
-    ];
-
-    return employeePayrollListLocal;
+const formatDate = (date) => {
+    let startDate = new Date(date);
+    const options = {
+        year: 'numeric', month: 'long', day: 'numeric'
+    };
+    const empDate = !startDate ? "undefined" : startDate.toLocaleDateString("en-IN", options);
+    return empDate;
 }
 
